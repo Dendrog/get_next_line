e320f673aa8145196e82906936a1ebd5da8884d8
Dendrog <goodshs335@gmail.com>
2024-03-15 22:53:52 +0900

    1

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jakim <jakim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 20:50:40 by jakim             #+#    #+#             */
+/*   Updated: 2024/03/15 22:01:43 by jakim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 20
+#endif
+
+#endif
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "get_next_line.h"
+
+int main()
+{
+    printf("%d",BUFFER_SIZE);
+}
